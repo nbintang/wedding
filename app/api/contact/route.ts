6f00a8014c64598290779e81fc23b4905ac09a39
nbintang <nbintangh@gmail.com>
2024-08-03 05:54:08 +0700

    updated all

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -3,42 +3,59 @@ import { prisma } from "@/config/prisma";
 import { NextResponse, NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
-  const { username, messages, assignment } = await req.json(); // Parse the JSON body
-  if (username === "" || messages === "" || assignment === "") {
-    return NextResponse.json({
-      success: false,
-      message: "request failed!",
-    });
-  }
   try {
-    if (username && messages && assignment) {
-      await prisma.user.create({
-        data: {
-          username,
-          messages,
-          assignment,
-        },
-      });
-      await transporter.sendMail({
-        ...mailOptions,
-        subject: `Pesan dari ${username} - ${assignment}`,
-        text: "test",
-        html: `<p> ${messages}</p>
-        <p>From: ${username}</p>`,
-      });
+    const { username, messages, assignment } = await req.json(); // Parse the JSON body
+    
+    if (!username || !messages || !assignment) {
       return NextResponse.json({
-        status: 200,
-        success: true,
-        message: "Message sent!",
+        success: false,
+        message: "Request failed! All fields are required.",
       });
     }
+
+    await prisma.user.create({
+      data: {
+        username,
+        messages,
+        assignment,
+      },
+    });
+
+    await transporter.sendMail({
+      ...mailOptions,
+      subject: `Pesan dari ${username} - ${assignment}`,
+      text: "test",
+      html: `<p>${messages}</p><p>From: ${username}</p>`,
+    });
+
+    return NextResponse.json({
+      status: 200,
+      success: true,
+      message: "Message sent!",
+    });
   } catch (error) {
-    console.log(error);
+    console.error(error);
+    return NextResponse.json({
+      success: false,
+      message: "An error occurred while processing your request.",
+    });
   }
 }
 
 export async function GET(req: NextRequest) {
-  const data = await prisma.user.findMany();
-  console.log(data);
-  return NextResponse.json({ success: true, message: "success", data });
+  try {
+    const data = await prisma.user.findMany();
+    console.log(data);
+    return NextResponse.json({
+      success: true,
+      message: "Data retrieved successfully",
+      data,
+    });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({
+      success: false,
+      message: "An error occurred while retrieving data.",
+    });
+  }
 }
