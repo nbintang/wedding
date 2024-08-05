fc43aacda17a3742ceb133cc44acb178630cc7ac
nbintang <nbintangh@gmail.com>
2024-08-06 00:10:07 +0700

    added animate bounce icon

--- components/Date/DateSections.tsx
+++ components/Date/DateSections.tsx
@@ -12,7 +12,7 @@ export default function DateSections() {
       <div className="bg-cream-300 w-full text-cream-900  flex flex-col gap-4 p-4 rounded-md">
         <div className=" flex flex-col items-center gap-2">
           <div className="flex gap-2">
-            <span className="w-7 h-7">
+            <span className="w-7 h-7 animate-bounce ">
               <RingsIcon />
             </span>
             <h1 className={`${parisienFont.className} text-3xl font-semibold `}>
@@ -24,8 +24,8 @@ export default function DateSections() {
           </p>
         </div>
         <div className="flex flex-col items-center gap-2">
-          <div className="flex gap-2">
-            <span className="w-7 h-7">
+          <div className="flex gap-2 ">
+            <span className="w-7 h-7 animate-bounce">
               {" "}
               <FlowersIcon />
             </span>
@@ -34,7 +34,7 @@ export default function DateSections() {
             </h1>
           </div>
           <p className="text-xl font-semibold">
-            Ahad | 08 September 2024 | 11.00 - 16.00 WIB
+            Ahad | 08 September 2024 | 11.00np - 16.00 WIB
           </p>
         </div>
       </div>
